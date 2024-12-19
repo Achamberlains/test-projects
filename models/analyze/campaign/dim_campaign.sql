bce6b0da30b75048c13fd570aaf630697746f3aa
Achamberlains <aaliyah_chambers@outlook.com>
2024-12-19 04:04:42 -0500

    additions

--- models/analyze/campaign/dim_campaign.sql
+++ models/analyze/campaign/dim_campaign.sql
@@ -14,8 +14,7 @@
 
         -- Attributes
         campaigns.campaign_name,
-        campaigns.campaign_channel,
-        ad_spend.campaign_date
+        campaigns.campaign_channel
     FROM {{ref('normalize_ad_spend')}} ad_spend
     LEFT JOIN {{ref('normalize_campaigns')}} campaigns
         ON ad_spend.campaign_id = campaigns.campaign_id
--- models/analyze/reports/campaign_obt.sql
+++ models/analyze/reports/campaign_obt.sql
@@ -8,16 +8,17 @@ WITH
 
 dim_campaign AS (
     SELECT
+        campaign_key,
         campaign_id,
         campaign_name,
-        campaign_channel,
-        campaign_date
+        campaign_channel
     FROM
         {{ ref('dim_campaign') }}
 ),
 
 fact_campaign AS (
     SELECT
+        campaign_key,
         campaign_id,
         campaign_ad_spend,
         campaign_conversion,
@@ -28,6 +29,7 @@ fact_campaign AS (
 
 final AS (
     SELECT
+        dim_campaign.campaign_key,
         dim_campaign.campaign_id,
         dim_campaign.campaign_name,
         dim_campaign.campaign_channel,
@@ -36,12 +38,11 @@ final AS (
         ROUND((fact_campaign.campaign_conversion * 100), 2) AS campaign_revenue,
         ROUND((fact_campaign.campaign_conversion/fact_campaign.campaign_ad_spend), 2) AS conversion_rate,
         ROUND(((fact_campaign.campaign_conversion * 100)/fact_campaign.campaign_ad_spend), 2) AS ROAS,
-        dim_campaign.campaign_date
+        fact_campaign.campaign_date
     FROM dim_campaign
     LEFT JOIN fact_campaign
-        ON dim_campaign.campaign_id = fact_campaign.campaign_id
-        AND dim_campaign.campaign_date = fact_campaign.campaign_date
-    QUALIFY ROW_NUMBER() OVER(PARTITION BY dim_campaign.campaign_id, dim_campaign.campaign_date ORDER BY dim_campaign.campaign_date) = 1
+        ON dim_campaign.campaign_key = fact_campaign.campaign_key
+    QUALIFY ROW_NUMBER() OVER(PARTITION BY dim_campaign.campaign_id, fact_campaign.campaign_date ORDER BY fact_campaign.campaign_date) = 1
 )
 
 SELECT * FROM final
